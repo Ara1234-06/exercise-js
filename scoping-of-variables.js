cfee6f86bd6b7b709552b7215f48dc06c88a852d
arafat <sar.arafat0607@gmail.com>
2025-09-11 14:42:54 +0200

    generated-even-numbers

---
+++ scoping-of-variables.js
@@ -0,0 +1,8 @@
+let outside = 14;
+{
+  let inside = 27;
+  console.log("inside block - outside" + outside);
+  console.log("inside block - indide" + inside);
+}
+console.log("out side block - outside" + outside);
+console.log("outsidde block - inside" + typeof inside);
