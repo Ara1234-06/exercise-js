9bcf47bc1a72632b6450a1cd184b8f8a2f7127ef
arafat <sar.arafat0607@gmail.com>
2025-09-11 15:47:38 +0200

    if-else

--- correct-input-required.js
+++ correct-input-required.js
@@ -1,7 +1,7 @@
 let word;
 
 do {
-  word = prompt("Enter a word (at least 5 characters):");
+  word = prompt("Enter a word (at least 5 characters)");
 } while (!word || word.length < 5);
 
 console.log("You entered:", word);
---
+++ if-else.js
@@ -0,0 +1,6 @@
+let number = 7;
+if (number > 5) {
+  console.log("the variable is greater than 5");
+} else {
+  console.log("the variable is less than 5");
+}
